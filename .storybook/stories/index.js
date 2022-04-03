def00eae1752d6372bdcdfe1f40f973d79e47e82
Jon Leahy <jonathanl@newtimesystems.com>
2022-04-04 00:39:41 +0100

    custom-button minor getting to work

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomButton from "../../src";
 import {storiesOf} from "@storybook/react";
+import CustomButton from "../../src/index";
 
 storiesOf("Custom button", module).addWithJSX("simple", () => <CustomButton/>)
